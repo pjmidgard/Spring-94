f36de08d321befdfbed51a50beec1cba8feae945
Java Software <pj.midgard@yahoo.com>
2022-04-02 21:04:59 +0100

    Update Spring-94.py

--- Spring-94.py
+++ Spring-94.py
@@ -322,7 +322,7 @@ class compression:
                                                     Number_Row1=Number_Row1+Number_Row6
                                                     
 
-                                            elif Number_Row2[0:1]==Number_Row_Count_str[0:1] and Number_Row2[0:2]!=Number_Row_Count_str and ei!=0:
+                                            elif Number_Row2[0:1]==Number_Row_Count_str[0:1] and ei!=0:
                                                     Number_Row6=Forest(Number_Row2,Number_Row_Count)
                                                     Number_Row1=Number_Row1+Number_Row6
                                                     if Number_Row6[0:1]==Number_Row_Count_str[0:1]:
